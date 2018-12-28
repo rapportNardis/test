a70436314c5a2aba35c5501c933dc1c5904f3091
rapportNardis <nardis1225@naver.com>
2018-12-28 19:26:22 +0900

    add_python_file

---
+++ 99dan.py
@@ -0,0 +1,8 @@
+def print_timesTable():
+    for i in range(2, 10):
+        print("\n", "===", i, "===")
+
+        for j in range(1, 10):
+            print(i, "x", j, "=", i*j)
+
+print_timesTable()
